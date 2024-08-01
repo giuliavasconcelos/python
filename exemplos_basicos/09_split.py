8a0a105aefcae928a1769b5effc229d28b980239
Giulia Vasconcelos <giuliavasconcelos620@gmail.com>
2024-08-01 13:52:19 -0300

    get

---
+++ exemplos_basicos/09_split.py
@@ -0,0 +1,7 @@
+mensagem = "Estou aprendendo Python no curso de DEV!"
+
+# Nova variavel que recebe o metodo split ()
+lista_mensag = mensagem.split(' ')
+
+# Exibir
+print(lista_mensag[2])
---
+++ exemplos_basicos/13_operadorOR1.py
@@ -0,0 +1,8 @@
+idade_livia = 17
+idade_matheus = 15
+
+#Operador OR (OU)
+if idade_livia >=18 or idade_matheus >=18:
+    print("Pelo menos um dos dois é maior de idade.")
+else:
+    print("Nenhum é maior de idade")
