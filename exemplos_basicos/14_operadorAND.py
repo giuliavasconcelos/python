5a4e47b567969d351916e6ce437db043bd005a58
Giulia Vasconcelos <giuliavasconcelos620@gmail.com>
2024-08-02 07:07:23 -0300

    ...

---
+++ exemplos_basicos/14_operadorAND.py
@@ -0,0 +1,8 @@
+idade_livia = 15
+idade_matheus = 18
+
+#Operador AND
+if idade_livia >=18 and idade_matheus >=18:
+    print("Pelo menos um dos dois é maior de idade.")
+else:
+    print("Nenhum é maior de idade")
